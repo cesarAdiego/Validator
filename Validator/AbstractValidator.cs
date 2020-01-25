f5b6e603c129e2ee75f61343bc23490e12e7b831
Cesar Adiego <caj2.santa@hotmail.com>
2020-01-25 13:05:51 +0100

    Agregar archivos de proyecto.

---
+++ Validator/AbstractValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Validator.Validator
+{
+    public class AbstractValidator<TEntity>
+    {
+        public List<Rule<TEntity>> Rules { get; set; }
+
+        private TEntity _entity { get; set; }
+
+        public AbstractValidator(TEntity entity) 
+        {
+            Rules = new List<Rule<TEntity>>();
+            _entity = entity;
+        }
+
+        public bool Validate()
+        {
+            bool result = true;
+
+            foreach(var rule in Rules)
+            {
+                result = result && rule.Validate(_entity);
+            }
+
+            return result;
+        }
+
+        public void AddRule(Func<TEntity, bool> func)
+        {
+            var rule = new Rule<TEntity>(func);
+            
+            Rules.Add(rule);
+        }
+    }
+}
---
+++ Validator/Rule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Validator.Validator
+{
+    public class Rule<TEntity>
+    {
+        private Func<TEntity, bool> _rule { get; set; }
+
+        public Rule(Func<TEntity, bool> rule)
+        {
+            _rule = rule;
+        }
+
+        public bool Validate(TEntity entity)
+        {
+            return _rule.Invoke(entity);
+        }
+    }
+}
