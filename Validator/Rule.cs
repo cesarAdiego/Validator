f09816438f76b99da87b2a4d55ada1f51a7b251c
Cesar Adiego <caj2.santa@hotmail.com>
2020-01-30 19:30:15 +0100

    Removed Rule commentaries

--- Validator/Rule.cs
+++ Validator/Rule.cs
@@ -37,20 +37,6 @@ namespace Validator.Validator
 
         public List<IRuleCondition<TOut>> Conditions { get; set; }
 
-        //public bool CheckIfNull { get; set; }
-
-        //public bool CheckIsBiggerThan { get; set; }
-
-        //public bool CheckIsLesserThan { get; set; }
-
-        //public bool CheckAreEqual { get; set; }
-
-        //public TOut BiggerThanObject { get; set; }
-
-        //public TOut LesserThanObject { get; set; }
-
-        //public TOut EqualObject { get; set; }
-
         public Rule(Expression<Func<TIn, TOut>> func)
         {
             _singleParam = func;
@@ -69,58 +55,7 @@ namespace Validator.Validator
                 result.MergeWith(ruleConditionResult);
             }
 
-            //if(CheckIfNull)
-            //{
-            //    ValidateParamIsNotNull(value, result);
-            //}
-
-            //if(CheckIsBiggerThan)
-            //{
-            //    ValidateParamIsBiggerThan(value, result);
-            //}
-
-            //if(CheckIsLesserThan)
-            //{
-            //    ValidateParamIsLesserThan(value, result);
-            //}
-
-            //if(CheckAreEqual)
-            //{
-            //    ValidateParamAreEqual(value, result);
-            //}
-
             return result;
         }
-
-        //private void ValidateParamIsNotNull(TOut value, ValidationResult validationResult)
-        //{
-        //    if(EqualityComparer<TOut>.Default.Equals(value, default))
-        //    {
-        //        validationResult.AddValidation(ParamName, "Is null");
-        //    }
-        //}
-
-        //private void ValidateParamIsBiggerThan(TOut value, ValidationResult validationResult)
-        //{
-        //    if(!(Comparer<TOut>.Default.Compare(value, BiggerThanObject) >= 0))
-        //    {
-        //        validationResult.AddValidation(ParamName, "Is not greater");
-        //    }
-        //}
-
-        //private void ValidateParamIsLesserThan(TOut value, ValidationResult validationResult)
-        //{
-        //    if(!(Comparer<TOut>.Default.Compare(value, LesserThanObject) <= 0))
-        //    {
-        //        validationResult.AddValidation(ParamName, "Is not less");
-        //    }
-        //}
-
-        //private void ValidateParamAreEqual(TOut value, ValidationResult validationResult)
-        //{
-        //    if(Comparer<TOut>.Default.Compare(value, EqualObject) != 0) {
-        //        validationResult.AddValidation(ParamName, "Are not equal");
-        //    }
-        //}
     }
 }
